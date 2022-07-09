30952355ab22ff22504a884df7db808beeebef8b
AngelBzDev <angelbaez.dev@gmail.com>
2022-07-09 15:53:10 -0500

    x

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Link to="/auth/login">Login</Link>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
