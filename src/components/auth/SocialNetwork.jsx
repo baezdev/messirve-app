e66c25772e0032b9cdf490e511338537107694d1
AngelBzDev <angelbaez.dev@gmail.com>
2022-07-10 22:12:37 -0500

    Change🤡: Mejora en estilos

--- src/components/auth/SocialNetwork.jsx
+++ src/components/auth/SocialNetwork.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-const SocialNetwork = ({ icon }) => {
+const SocialNetwork = ({ icon, onClick }) => {
   return (
-    <div className="flex items-center justify-center bg-white border rounded-lg border-gray2 h-11">
+    <div
+      className="flex items-center justify-center duration-200 bg-white border rounded-lg cursor-pointer border-gray2 h-14 hover:-translate-y-1"
+      onClick={onClick}
+    >
       {icon}
     </div>
   );
