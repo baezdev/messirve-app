3e3f8bb27fa0ff7b93d59ac40f310735be7a29c3
AngelBzDev <angelbaez.dev@gmail.com>
2022-07-10 22:21:08 -0500

    Create🥸: Archivo que maneja el estado de la app

---
+++ src/store/store.js
@@ -0,0 +1,22 @@
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+import authReducer from "../reducers/authReducer";
+
+const composeEnhancers =
+  (typeof window !== "undefined" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+//Adjunta los reducers para poder usarlos en el createStore
+const reducers = combineReducers({
+  //La estructura de este objeto es como se vera el state
+  auth: authReducer,
+});
+//Para crear un store usamos createStore al cual se le pasa SOLO un reducer como parametro
+export const store = createStore(
+  reducers,
+  //Esto nos permite trabajar con acciones asincronas
+  composeEnhancers(applyMiddleware(thunk))
+);
+//Despues lo importamos en el punto mas alto de la aplicacion
