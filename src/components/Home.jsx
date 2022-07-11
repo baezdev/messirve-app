40a0da09508084e4d3af2c43d7d28b7162901a33
AngelBzDev <angelbaez.dev@gmail.com>
2022-07-11 14:55:36 -0500

    Add🤠: cerrar sesion

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { startLogout } from "../action/auth";
+import { useDispatch } from "react-redux";
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(startLogout());
+  };
+
   return (
     <div>
       <Link to="/auth/login">Login</Link>
+      <br />
+      <button onClick={handleLogout}>Cerrar Sesión</button>
     </div>
   );
 };
