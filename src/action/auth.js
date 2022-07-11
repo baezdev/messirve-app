db1a2b05a0415ea1a7f1e139a63e13a5e57919db
AngelBzDev <angelbaez.dev@gmail.com>
2022-07-11 14:53:44 -0500

    Add🤠: registro, inicio de sesion, cerrar sesin

--- src/action/auth.js
+++ src/action/auth.js
@@ -4,6 +4,8 @@ import {
   signInWithPopup,
   updateProfile,
 } from "firebase/auth";
+import Swal from "sweetalert2";
+
 import { auth, googleProvider } from "../firebase/firebase.config";
 import types from "../types/types";
 
@@ -25,5 +27,63 @@ export const startWithGoogle = () => {
     signInWithPopup(auth, googleProvider).then(({ user }) =>
       dispatch(login(user.uid, user.displayName))
     );
+  };
+};
+
+export const startLoginEmailPassword = (email, password) => {
+  return (dispatch) => {
+    /* dispatch(startLoading()); */
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then(({ user }) => {
+        dispatch(login(user.uid, user.displayName));
+
+        /* dispatch(finishLoading()); */
+      })
+      .catch((e) => {
+        Swal.fire({
+          title: "Ups!!",
+          icon: "error",
+          text: `${
+            e.code === "auth/user-not-found"
+              ? "El usuario no existe"
+              : "La contraseña es incorrecta"
+          }`,
+        });
+        /* dispatch(finishLoading()); */
+      });
+  };
+};
+
+export const startCreateUserWithEmailAndPassword = (email, password, name) => {
+  return (dispatch) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(async ({ user }) => {
+        //Actualizar los datos del perfil
+        await updateProfile(user, {
+          displayName: name,
+        });
+        dispatch(login(user.uid, user.displayName));
+      })
+      .catch((e) => {
+        Swal.fire({
+          title: "Ups!!",
+          icon: "error",
+          text: `${
+            e.code === "auth/email-already-in-use"
+              ? "El correo ya esta en uso"
+              : e.message
+          }`,
+        });
+      });
+  };
+};
+
+export const startLogout = () => {
+  return async (dispatch) => {
+    await auth.signOut();
+    dispatch(logout());
+    //Aqui se limpia al cerrar sesion
+    /*     dispatch(noteLogout()); */
   };
 };
