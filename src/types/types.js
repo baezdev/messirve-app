64843de9c46a70cb7864efafd8bb07449bc713f7
AngelBzDev <angelbaez.dev@gmail.com>
2022-07-10 22:20:27 -0500

    Create🥸: Tipos de acciones

---
+++ src/types/types.js
@@ -0,0 +1,14 @@
+//2. Este archivo define los typos y la accion que hace cada uno
+
+const types = {
+  //Autenticacion
+  login: "[Auth] Login",
+  logout: "[Auth] Logout",
+  //Interfaz
+  /* setError: "[UI] Set Error",
+  removeError: "[UI] Remove Error",
+  uiStartLoading: "[UI] Start loading",
+  uiFinishLoading: "[UI] Finish loading", */
+};
+
+export default types;
