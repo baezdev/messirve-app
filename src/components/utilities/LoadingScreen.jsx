f38699708a51f9351adf36408f62630ec3f5b54f
AngelBzDev <angelbaez.dev@gmail.com>
2022-07-11 14:55:58 -0500

    Change🤡: Tamaño de imagen

--- src/components/utilities/LoadingScreen.jsx
+++ src/components/utilities/LoadingScreen.jsx
@@ -4,7 +4,7 @@ import logo from "../../images/logo.svg";
 const LoadingScreen = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen">
-      <Image src={logo} alt="logo" width={300} />
+      <Image src={logo} alt="logo" width={200} />
       <span className="loader"></span>
     </div>
   );
