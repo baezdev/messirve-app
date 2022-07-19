de06f81dfdc4a02f08135f41b9f67939eb509aa4
AngelBzDev <angelbaez.dev@gmail.com>
2022-07-19 16:51:50 -0500

    Config🔧: Rutas privadas

--- src/routes/MessirveRouter.jsx
+++ src/routes/MessirveRouter.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomeScreen from "../components/home/HomeScreen";
+import ProductScreen from "../components/products/ProductScreen";
 import SellScreen from "../page/SellScreen";
 import ShoppingCart from "../page/ShoppingCart";
 
@@ -9,6 +10,7 @@ const MessirveRouter = () => {
     <Routes>
       <Route path="shopping-cart" element={<ShoppingCart />} />
       <Route path="sell" element={<SellScreen />} />
+      <Route path="product/:idProduct" element={<ProductScreen />} />
       <Route path="*" element={<HomeScreen />} />
     </Routes>
   );
