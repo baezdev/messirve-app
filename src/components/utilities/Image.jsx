2f0b035078c42f7f9a5e8553415e8e7baeebe2ec
AngelBzDev <angelbaez.dev@gmail.com>
2022-07-19 16:42:21 -0500

    Changes🤡: Estilos de la imagen

--- src/components/utilities/Image.jsx
+++ src/components/utilities/Image.jsx
@@ -1,7 +1,7 @@
 const Image = ({ src, alt, width, className }) => {
   return (
     <div className={`w-full mb-6 ${className}`}>
-      <img src={src} alt={alt} width={width} className="mx-auto"/>
+      <img src={src} alt={alt} width={width} className="mx-auto rounded-md"/>
     </div>
   );
 };
