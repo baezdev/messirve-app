dfd71c43776ad6dcea75621a0dee6a3a68ffa8e2
AngelBzDev <angelbaez.dev@gmail.com>
2022-07-19 16:38:12 -0500

    Create🥸: Componente que contiene la lista de productos

---
+++ src/components/products/Products.jsx
@@ -0,0 +1,31 @@
+import { useSelector } from "react-redux";
+
+import Container from "../utilities/Container";
+import ProductCard from "./ProductCard";
+
+const Products = () => {
+  const { products } = useSelector(({ product }) => product);
+
+  return (
+    <section className="mb-16 animate__animated animate__fadeInUp">
+      <Container>
+        <h3 className="my-6 text-2xl font-bold md:text-3xl">
+          Nuevos Productos
+        </h3>
+        <div className="grid grid-cols-2 gap-5 md:grid-cols-4">
+          {products?.map(({ id, nameProduct, price, pictures }) => (
+            <ProductCard
+              key={nameProduct}
+              image={pictures[0]}
+              price={price}
+              name={nameProduct}
+              id={id}
+            />
+          ))}
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default Products;
