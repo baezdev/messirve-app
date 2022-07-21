49687a0e81368f434f0baff2774f5d6c346ad350
AngelBzDev <angelbaez.dev@gmail.com>
2022-07-20 20:44:03 -0500

    Add🤠: helper para traer productos por id

--- src/helpers/loadProducts.js
+++ src/helpers/loadProducts.js
@@ -1,7 +1,7 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, doc, getDocs, query, where } from "firebase/firestore";
 import { db } from "../firebase/firebase.config";
 
-const loadProducts = async () => {
+export const loadProducts = async () => {
   //Consulta a la base de datos
   const docSnap = await getDocs(collection(db, "/products"));
   const products = [];
@@ -15,4 +15,18 @@ const loadProducts = async () => {
   return products;
 };
 
-export default loadProducts;
+export const loadProductById = async (id) => {
+  //TODO Refactorizar esta funcion
+  /* const q = query(collection(db, `/products/`), where("uid", "==", "gbKXruXtAoUulY7nvoI4"));
+  const querySnapshot = await getDocs(q);
+  const products = [];
+  querySnapshot.forEach((product) =>
+    products.push({
+      id: product.id,
+      ...product.data(),
+    })
+  ); */
+  const products = await loadProducts();
+
+  return products.filter((p) => p.id === id);
+};
