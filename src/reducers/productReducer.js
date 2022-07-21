2b48234639f87d427dd0023f5f5d7be67fec8051
AngelBzDev <angelbaez.dev@gmail.com>
2022-07-20 20:41:45 -0500

    Add🤠: reducer de productos

--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -2,13 +2,20 @@ import types from "../types/types";
 
 const init = {
   products: [],
+  productActive: null,
 };
 
 const productReducer = (state = init, action) => {
   switch (action.type) {
     case types.productsLoad:
       return {
+        ...state,
         products: [...action.payload],
+      };
+    case types.productActive:
+      return {
+        ...state,
+        productActive: { ...action.payload },
       };
     default:
       return state;
