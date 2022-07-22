d7ab6e7137518302584ca7645cdd3b98bbec3050
AngelBzDev <angelbaez.dev@gmail.com>
2022-07-22 02:13:18 -0500

    Add🤠: accion para añadir productos

--- src/action/product.js
+++ src/action/product.js
@@ -1,5 +1,11 @@
-import { loadProducts, loadProductById } from "../helpers/loadProducts";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../firebase/firebase.config";
+
+import Swal from "sweetalert2";
+
 import types from "../types/types";
+import { fileUpload } from "../helpers/fileUpload";
+import { loadProducts, loadProductById } from "../helpers/loadProducts";
 
 export const setProducts = (products) => ({
   type: types.productsLoad,
@@ -24,4 +30,32 @@ export const startActiveProduct = (id) => {
     const product = await loadProductById(id);
     dispatch(setActiveProduct(product));
   };
+};
+
+export const startNewProduct = async (product) => {
+  const { pictures } = product;
+  const arrPictures = await Promise.all(
+    pictures.map(async (picture) => {
+      return await fileUpload(picture);
+    })
+  );
+
+  const newProduct = {
+    ...product,
+    pictures: arrPictures,
+  };
+  await addDoc(collection(db, "products"), newProduct);
+  Swal.fire({
+    title: "Exito! 🤑",
+    html: "El producto se publicó exitosamente",
+    icon: "success",
+    buttonsStyling: false,
+    customClass: {
+      confirmButton: "button",
+    },
+  }).then((result) => {
+    if (result.isConfirmed) {
+      window.location.href = "/";
+    }
+  });
 };
